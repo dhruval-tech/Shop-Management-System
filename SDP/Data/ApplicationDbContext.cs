11468085d17514b3eadd45ba5cbf1989f4cb0221
dhruval-tech <dhruvalgandhi2000@gmail.com>
2021-01-02 23:59:38 +0530

    Added modules successfully

---
+++ SDP/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SDP.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SDP.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<product> products { get; set; }
+        public DbSet<customer> customers { get; set; }
+    }
+}
---
+++ SDP/Models/staff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SDP.Models
+{
+    //(worker_email, worker_name, worker_contact, worker_sallery,
+    //worker_address, role)
+
+    public class staff
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string name { get; set; }
+        public int contact { get; set; }
+        public int salary { get; set; }
+        public string address { get; set; }
+        public string role { get; set; }
+    }
+}
