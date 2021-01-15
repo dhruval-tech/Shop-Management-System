5883a9a4fa7a74ab3af33b81820785c8a46439f9
Naiya Prajapti <naiyaprajapti2000@gmail.com>
2021-01-15 22:55:53 +0530

    Product CRUD Added Successfully

---
+++ SDP/Data/Migrations/20210115062406_ProductModule.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SDP.Data.Migrations
+{
+    public partial class ProductModule : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isPurchase",
+                table: "products");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "products",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "products");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isPurchase",
+                table: "products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
